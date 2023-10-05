38cfeb84a15303222e5f0226bff5d867c80dcb41
Matthew Lewis <matthew.a.lewis7@gmail.com>
2023-10-05 15:26:33 -0400

    feat: add page builder schema for sanity.

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -3,7 +3,9 @@ import post from './schemas/post';
 import author from './schemas/author';
 import blockContent from './schemas/blockContent';
 import category from './schemas/category';
+import page from './schemas/page';
+import hero from './schemas/hero';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, blockContent, category],
+  types: [post, author, blockContent, category, page, hero],
 };
---
+++ sanity/schemas/page.ts
@@ -0,0 +1,31 @@
+import { defineArrayMember, defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'page',
+  type: 'document',
+  title: 'Page',
+  fields: [
+    defineField({ name: 'title', type: 'string' }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      validation: (Rule) => Rule.required(),
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'pageBuilder',
+      type: 'array',
+      title: 'Page builder',
+      of: [
+        defineArrayMember({
+          name: 'hero',
+          type: 'hero',
+        }),
+      ],
+    }),
+  ],
+});

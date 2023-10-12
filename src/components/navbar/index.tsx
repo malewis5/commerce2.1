0de3e6649eaa7d52eb498e8606298d8ac184f1dd
Matthew Lewis <matthew.a.lewis7@gmail.com>
2023-10-12 00:03:45 -0500

    feat: close mobile menu on search

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -7,21 +7,21 @@ import MobileMenu from './mobile-menu';
 import { sanityFetch } from '@/lib/sanity/sanity-fetch';
 import { SanityDocument } from 'next-sanity';
 import { navigationQuery } from '@/lib/sanity/queries';
+import { Skeleton } from '@/components/ui/skeleton';
+import { MenuIcon } from 'lucide-react';
 
 export async function Navbar() {
   const { navItems } = await sanityFetch<
-    SanityDocument<{ navItems: { title: string; slug: string }[] }>
+    SanityDocument<{ navItems?: { title: string; slug: string }[] }>
   >({
     query: navigationQuery,
     params: { tag: 'main' },
     tags: ['navigation'],
   });
 
-  console.log(navItems);
-
   return (
     <nav className='relative flex items-center justify-between p-4 lg:px-6 mb-8 shadow-md bg-white'>
-      <Suspense>
+      <Suspense fallback={<MenuIcon className='lg:hidden' />}>
         <MobileMenu navItems={navItems} />
       </Suspense>
       <div className='hidden lg:flex w-full items-center'>
@@ -38,18 +38,19 @@ export async function Navbar() {
         </div>
         <div className='flex w-full md:w-1/3 justify-end'>
           <div className='hidden lg:flex space-x-5 xl:space-x-10 items-center'>
-            <Suspense>
-              {navItems.map((item) => {
-                return (
-                  <Link
-                    key={item.slug}
-                    className='text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300'
-                    href={item.slug}
-                  >
-                    {item.title}
-                  </Link>
-                );
-              })}
+            <Suspense fallback={<Skeleton className='w-52 h-6' />}>
+              {navItems &&
+                navItems.map((item) => {
+                  return (
+                    <Link
+                      key={item.slug}
+                      className='text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300'
+                      href={item.slug}
+                    >
+                      {item.title}
+                    </Link>
+                  );
+                })}
             </Suspense>
             <div className='hidden md:flex'>
               <Suspense fallback={<OpenCart />}>

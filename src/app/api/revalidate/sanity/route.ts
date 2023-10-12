c68a886ec8f1cd548d056a880dd11c674a22ca8f
Matthew Lewis <matthew.a.lewis7@gmail.com>
2023-10-12 09:12:29 -0500

    fix: sanity unable to run on edge.

--- src/app/api/revalidate/sanity/route.ts
+++ src/app/api/revalidate/sanity/route.ts
@@ -4,8 +4,6 @@ import { NextResponse } from 'next/server';
 
 const secret = process.env.SANITY_CONTENT_PAGE_WEBHOOK_SECRET ?? '';
 
-export const runtime = 'edge';
-
 export async function POST(request: Request) {
   const topic = request.headers.get('x-sanity-topic');
   const signature = request.headers.get(SIGNATURE_HEADER_NAME) || '';
